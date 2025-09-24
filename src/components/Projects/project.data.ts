ba2691ee793c9f08d31003292dcbc57499d50f59
MatheusKerscher <kerschermatheus12@gmail.com>
2025-09-24 20:01:42 -0300

    Update projects section

--- src/components/Projects/project.data.ts
+++ src/components/Projects/project.data.ts
@@ -4,36 +4,41 @@ export const projects: Project[] = [
   {
     title: "Pet na porta",
     cover: "images/thumbnail-pet-na-porta.png",
-    description: "Website de produtos para pet destinado a listar produtos, visualizar detalhes do produto e gerenciar o carrinho do cliente",
+    description:
+      "Website de produtos para pet destinado a listar produtos, visualizar detalhes do produto e gerenciar o carrinho do cliente",
     githubLink: "https://github.com/MatheusKerscher/pet-na-porta",
-    websiteLink: "https://pet-na-porta.vercel.app/"
+    websiteLink: "https://pet-na-porta.vercel.app/",
   },
   {
     title: "Web Carros",
     cover: "images/thumbnail-web-carros.png",
-    description: "Projeto com uma loja de carros, permitindo visualizar os carros, criar uma conta e cadastrar, editar ou remover um carro",
+    description:
+      "Projeto com uma loja de carros, permitindo visualizar os carros, criar uma conta e cadastrar, editar ou remover um carro",
     githubLink: "https://github.com/MatheusKerscher/web-carros",
-    websiteLink: "https://web-carros-pi.vercel.app/"
+    websiteLink: "https://web-carros-pi.vercel.app/",
   },
   {
     title: "Cripto Currency",
     cover: "images/thumbnail-cripto-currency.png",
-    description: "Site para exibir a cotação e detalhes de criptmoedas consumindo uma API externa",
+    description:
+      "Site para exibir a cotação e detalhes de criptmoedas consumindo uma API externa",
     githubLink: "https://github.com/MatheusKerscher/cripto-currency",
-    websiteLink: "https://cripto-currency-orcin.vercel.app/"
+    websiteLink: "https://cripto-currency-orcin.vercel.app/",
   },
   {
-    title: "Portfólio",
-    cover: "images/thumbnail.png",
-    description: "Website contendo as linguagens e frameworks que uso, meus melhores projetos e canais de comunicação comigo",
-    githubLink: "https://github.com/MatheusKerscher/portfolio",
-    websiteLink: "https://portfolio-theta-orpin-65.vercel.app/"
+    title: "Dev Controle",
+    cover: "images/thumbnail-dev-controle.png",
+    description:
+      "Aplicação desenvolvida com NextJS para gerenciar atendimentos e clientes de forma eficiente",
+    githubLink: "https://github.com/MatheusKerscher/dev-controle",
+    websiteLink: "https://dev-controle-topaz.vercel.app/",
   },
   {
     title: "MyMock",
     cover: "images/thumbnail-my-mock.png",
-    description: "Aplicação para geração de dados mock feito com StyledComponents, i18n (internacionalização) e troca de tema",
+    description:
+      "Aplicação para geração de dados mock feito com StyledComponents, i18n (internacionalização) e troca de tema",
     githubLink: "https://github.com/MatheusKerscher/my-mock",
-    websiteLink: "https://my-mock-ecru.vercel.app/"
+    websiteLink: "https://my-mock-ecru.vercel.app/",
   },
-]
+];
