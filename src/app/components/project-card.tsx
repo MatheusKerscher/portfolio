d91f861eeb28e796d10e75ade07c01186476f6ae
MatheusKerscher <kerschermatheus12@gmail.com>
2025-11-23 14:43:44 -0300

    feat: add projects in project tab

--- src/app/components/project-card.tsx
+++ src/app/components/project-card.tsx
@@ -1,16 +1,43 @@
 import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 
-const ProjectCard = () => {
+type ProjectCardProps = {
+  title: string;
+  imageUrl: string;
+  repositoryUrl?: string;
+  websiteUrl?: string;
+};
+
+const ProjectCard = ({
+  title,
+  imageUrl,
+  repositoryUrl,
+  websiteUrl,
+}: ProjectCardProps) => {
   return (
-    <Card>
-      <CardContent>
-        <p>Imagem do projeto</p>
-      </CardContent>
+    <a
+      href={websiteUrl || repositoryUrl}
+      target="_blank"
+      className="group block hover:scale-105 transition-transform duration-200 ease-in-out"
+    >
+      <Card className="overflow-hidden">
+        <CardContent>
+          <div className="relative w-full h-56 overflow-hidden">
+            <Image
+              src={imageUrl}
+              alt={title}
+              priority
+              fill
+              className="object-cover transition-transform duration-500 group-hover:scale-105"
+            />
+          </div>
+        </CardContent>
 
-      <CardFooter>
-        <CardTitle>Projeto</CardTitle>
-      </CardFooter>
-    </Card>
+        <CardFooter>
+          <CardTitle>{title}</CardTitle>
+        </CardFooter>
+      </Card>
+    </a>
   );
 };
 
