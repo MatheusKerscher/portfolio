740d4e4e76f0b93ff37e02158ef98969e38abfbe
MatheusKerscher <kerschermatheus12@gmail.com>
2025-11-23 14:44:48 -0300

    refactor: adjust footer width

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-screen border-t border-gray-300 text-sm select-none">
+    <footer className="w-full border-t border-gray-300 text-sm select-none">
       <div className="w-screen max-w-6xl mx-auto flex flex-wrap gap-4 justify-between p-3 lg:px-1">
         <p>&copy; {currentYear}</p>
         <p>Feito com ❤️ por Matheus Kerscher</p>
