631e89bb8536628ec17f467920694e837dc01a95
MatheusKerscher <kerschermatheus12@gmail.com>
2026-06-04 13:01:13 -0300

    feat: add seo infrastructure (sitemap, robots, og image, json-ld, llms.txt)

---
+++ src/app/robots.ts
@@ -0,0 +1,9 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: "https://kerscher.dev.br/sitemap.xml",
+    host: "https://kerscher.dev.br",
+  };
+}
